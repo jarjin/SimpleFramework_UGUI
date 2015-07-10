6c9855ce5f6ccfc9919145740a81d6490c0f91e3
jarjin <2338894521@qq.com>
2015-07-11 00:54:50 +0800

    调整Lua代码目录结构

--- Assets/Lua/Build.bat
+++
@@ -1,9 +0,0 @@
-cd /d %~dp0
-cd out
-del /s *.txt
-del /s *.bytes
-cd..
-for %%i in (*.lua) do luajit -b %%i out\%%i
-cd out
-ren *.lua *.lua.bytes
-cd..
--- Assets/Lua/common/define.lua
+++
@@ -1,9 +0,0 @@
-Util = SimpleFramework.Util;
-AppConst = SimpleFramework.AppConst;
-LuaHelper = SimpleFramework.LuaHelper;
-ByteBuffer = SimpleFramework.ByteBuffer;
-
-ResManager = LuaHelper.GetResManager();
-NetManager = LuaHelper.GetNetManager();
-PanelManager = LuaHelper.GetPanelManager();
-MusicManager = LuaHelper.GetMusicManager();
